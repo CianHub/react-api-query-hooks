5832a96ff1ea9233ffe6247467d74c4eae5bd610
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 22:27:04 +0100

    Add search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Search = (props) => {
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for a character"
+        ></input>
+      </form>
+    </section>
+  );
+};
+
+Search.propTypes = {};
+
+export default Search;
