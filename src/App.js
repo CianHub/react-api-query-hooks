be59e959653538fdb09456183fadb4c2c4839d4a
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 20:32:27 +0100

    COnvert App component to arrow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div className="container">Hello</div>;
+};
 
 export default App;
