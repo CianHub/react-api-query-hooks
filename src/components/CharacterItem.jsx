503bdc8cb0403601cac156cd1d8313eb261b392d
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 22:19:36 +0100

    Character item

---
+++ src/components/CharacterItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CharacterItem = (props) => {
+  return <div></div>;
+};
+
+CharacterItem.propTypes = {};
+
+export default CharacterItem;
