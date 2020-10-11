06706a61f6fd44c3bb2999fc838b91adff68900c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 22:10:56 +0100

    Add useEffect hook to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import axios from 'axios';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect;
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const result = await axios.get(`https://www.breakingbadapi.com/api/`);
+      console.log(result.data);
+    };
+    fetchCharacters();
+  }, []);
+
   return (
     <div className="container">
       <Header></Header>
